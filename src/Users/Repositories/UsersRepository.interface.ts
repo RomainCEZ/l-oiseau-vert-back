d4712f7e7abeb400cf3a656f3a550a58dc7eb0d0
Romain <hercez92@hotmail.fr>
2022-06-29 01:15:24 +0200

    make isUnique method optional

--- src/Users/Repositories/UsersRepository.interface.ts
+++ src/Users/Repositories/UsersRepository.interface.ts
@@ -3,6 +3,6 @@ import User from "../entities/User"
 export default interface IUsersRepository {
     getUserByEmail(email: string): Promise<User>
     getUserById(id: string): Promise<User>
-    isUnique(newUser: User): Promise<boolean>
+    isUnique?(newUser: User): Promise<boolean>
     saveUser(newUser: User): Promise<void>
 }
