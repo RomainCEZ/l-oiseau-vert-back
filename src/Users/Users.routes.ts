77f58878a57054c665fc97e49c9db6edbde0a1b9
Romain <hercez92@hotmail.fr>
2022-06-29 18:18:55 +0200

    move route

--- src/Users/Users.routes.ts
+++ src/Users/Users.routes.ts
@@ -6,8 +6,8 @@ import usersController from "./Users.controller";
 const router = Router();
 
 router.post('/register', usersController.register);
+router.delete('/logout', usersController.logout);
 router.post('/login', passport.authenticate('local'), usersController.login);
 router.get('/', LocalAuth.isAuthenticated, usersController.relog);
-router.delete('/logout', usersController.logout);
 
 export { router };
